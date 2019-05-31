9e124f47c4e131530276997e558ed6057b6da36a
DESKTOP-6ST7TKK\michaelerdmann <michael.erdmann@snafu.de>
2019-05-31 17:13:10 +0200

    Firsr paet of the lamnding pages

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
+import "bootstrap/dist/css/bootstrap.css";
 import React, { Component } from "react";
 import "./App.css";
-import Button from "react-bootstrap/Button";
+import logo from "./MYtineraryLogo.png"; // with import
+import Home from "./home.png";
+import HomeActive from "./homeActive.png";
+
+class Login extends Component {
+  State = {
+    userName: null
+  };
+  render() {
+    return (
+      <img
+        src={this.state.userName == null ? Home : HomeActive}
+        className="icon"
+        alt=""
+      />
+    );
+  }
+}
 
 class LandingPage extends Component {
-  state = {};
   render() {
     return (
       <div>
-        <div>
-          <h1>MyIternery</h1>
-          One morning, when Gregor Samsa woke from troubled dreams, he found
-          himself transformed in his bed into a horrible vermin. He lay on his
-          armour-like back, and if he lifted his head a little he could see his
-          brown belly, slightly domed and divided by arches into stiff sections.
-          The bedding was hardly able to cover it and seemed ready to slide off
-          any moment. His many legs, pitifully thin compared with the size of
-          the rest of him, waved about helplessly as he looked. "What's happened
-          to me?" he thought. It wasn't a dream.
-        </div>
-        <button type="button" class="btn btn-danger">
-          Primary
-        </button>
-        <button>Sign in</button>
-        <button>Signup</button>
-        <button>Browse</button>
+        <login />
+        <div className="centered">
+          <img className="logo" src={logo} alt="" />
+          <p className="Greeting">
+            Find your perfect trip, designed by insiders who know and love their
+            cities.{" "}
+          </p>{" "}
+        </div>{" "}
       </div>
     );
   }
