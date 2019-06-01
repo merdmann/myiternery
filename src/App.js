64dc04b058480b535c86863925b44556ef430e2c
Michael Erdmann <michael.erdmann@snafu.de>
2019-06-01 19:59:21 +0200

    weekend SYNC

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
 import "bootstrap/dist/css/bootstrap.css";
+import PropTypes from 'prop-types';
 import React, { Component } from "react";
 import "./App.css";
 import logo from "./MYtineraryLogo.png"; // with import
-import Home from "./home.png";
-import HomeActive from "./homeActive.png";
-
-class Login extends Component {
-  State = {
-    userName: null
-  };
-  render() {
-    return (
-      <img
-        src={this.state.userName == null ? Home : HomeActive}
-        className="icon"
-        alt=""
-      />
-    );
-  }
-}
+import home from "./home.png";
+import homeActive from "./homeActive.png";
+import burger  from "./menu-button-of-three-horizontal-lines.png";
 
 class LandingPage extends Component {
   render() {
     return (
-      <div>
-        <login />
-        <div className="centered">
+        <div>
+        <Section>
+              <Header />
+              <Burger />
+        </Section>
+          <div className="centered">
           <img className="logo" src={logo} alt="" />
-          <p className="Greeting">
+            <p className="Greeting">
             Find your perfect trip, designed by insiders who know and love their
-            cities.{" "}
-          </p>{" "}
-        </div>{" "}
-      </div>
-    );
+            cities.
+            </p>
+          </div>
+          </div>
+    )
   }
 }
 
+class Burger extends React.Component {
+  render() {
+    return <img className="icon right" src={burger} alt=""/>;
+  }
+}
+
+
+//
+// a simple fuctional component
+//
+function Header(props) {
+ 
+  return <div className="Header"></div>;
+}
+
 export default LandingPage;
+
+class Section extends React.Component {
+  getChildContext() {
+    return { some: "context" };
+  }
+
+  render() {
+    return <div className="Section">{this.props.children}</div>;
+  }
+}
