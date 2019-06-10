d1f8b1fa0aa3bb52e21311ba3e2e9a86cc866ffc
Michael Erdmann <michael.erdmann@snafu.de>
2019-06-10 17:11:46 +0200

    Adding a new gir package

--- src/components/visit.js
+++ src/components/visit.js
@@ -1,13 +1,23 @@
 /**
  * It prvides the visit link
  */
+import { Container, Row, Col } from 'react-grid-system';
 import React from "react";
+import Home from "./home.png"
+import activeHome from "./homeActive.png"
+
 import circle_right from "./circledright.png"
 
  function Visit(prop) {
      return (
-       <img src={circle_right} className="large-icon" alt=""></img> )
- }
-
+    <Container>
+    <Row>
+      <Col></Col>
+      <Col><img src={circle_right} className="large-icon" alt=""></img></Col>
+      <Col></Col>
+    </Row>
+    </Container>
+    );
+ }/* Visit */
 
  export default Visit;
