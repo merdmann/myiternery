888ff33085b12f51adb7e8fc210f153ccd8a0409
Michael Erdmann <michael.erdmann@snafu.de>
2019-06-16 17:12:02 +0200

    BSON package added to get documents

---
+++ backend/mongoshell.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mongo "mongodb+srv://cluster0-z60nm.azure.mongodb.net/test" --username merdmann
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,45 @@
 var express = require('express');
 var app = express();
+const BSON = require('bson');
+const Long = BSON.Long;
+var mongoose = require('mongoose'); 
+mongoose.connect("mongodb+srv://merdmann:Dieter1234@cluster0-z60nm.azure.mongodb.net/MyTinerary?retryWrites=true&w=majority");
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected")
+
+const  Users = db.collection("Users")
+
+function AddUser( name, secret ) {
+  let error = null;
+  Users.insertOne({fullname: name, "secret": secret }, 
+    function( err, doc ) { error = err });
+
+  return error;
+}
+
+AddUser( "Marina Massalsiki", 4713 );
+
+function UserExists(name, secret) {
+  const query = {fullname: "Michael Erdmann"}; 
+  let instance = 0;
+  Users.find(query).toArray( function( err, doc){ 
+    let o = BSON.serialize(doc);
+    console.log(BSON.deserialize(o));
+    instance += o[0].secret != null ? 1 : 0;
+  })
+  console.log(instance)
+  return instance;
+}
+
+UserExists("Michael Erdmann")
+
+
+db.close()
+}) /* db open end */ 
+ 
 app.get('/test/', function (req, res) {
   res.send('HALLO WORLD!');
 });
