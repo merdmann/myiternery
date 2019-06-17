0bc7702946398cfedf5b6048b0cb1e0f96541bd0
Michael Erdmann <michael.erdmann@snafu.de>
2019-06-17 14:33:36 +0200

    Trying to initialze the cities route with the database handle.

--- backend/src/routes/api/cities.js
+++ backend/src/routes/api/cities.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const BSON = require('bson');
+const Long = BSON.Long;
+const mongoose = require('mongoose'); 
+let   Cities = null;
 
-router.get("/", (req, res) => {
-  res.send("all my cities");
-});
+const initialize = function (aCities) {
+    Cities = aCities;
+}
+
+router.get("/1", (req, res) => {
+  let result = [];
+  Cities.find().toArray(function(err, docs) {
+    let o = BSON.serialize(docs);
+    let oo = BSON.deserialize(o);
+    result.push(oo);
+    console.log(result)
+    res.send(result);
+  });
+})
 
 router.get("/1", (req, res) => {
   res.send("city number 1");
 });
 
+
+module.exports = initialize;
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,50 +8,18 @@ const BSON = require('bson');
 const Long = BSON.Long;
 var mongoose = require('mongoose'); 
 mongoose.connect("mongodb+srv://merdmann:Dieter1234@cluster0-z60nm.azure.mongodb.net/MyTinerary?retryWrites=true&w=majority");
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   //+++++++++ Connected to db call back ++++++++++++++++++++++++
   console.log("Connected with db")
+  app.use(bodyParser.json());
+  app.use(cors());
 
-const  Users = db.collection("Users")
+  citiesRoute.initialize(db)
 
-function AddUser( name, secret ) {
-  let error = null;
-  Users.insertOne({fullname: name, "secret": secret }, 
-    function( err, doc ) { error = err });
+  app.use("/api/cities", citiesRoute);
 
-  return error;
-}
-
-AddUser( "Marina Massalsiki", 4713 );
-
-function UserExists(name, secret) {
-  const query = {fullname: "Michael Erdmann"}; 
-  let instance = 0;
-  Users.find(query).toArray( function( err, doc){ 
-    let o = BSON.serialize(doc);
-    let oo = BSON.deserialize(o);
-    console.log(oo[0]._id)
-    console.log(oo[0].secret)
-  })
-  return instance;
-}
-
-UserExists("Michael Erdmann")
-
-db.close()
+  db.close()
 }) /*+++++++++++++++++ db open end++++++++++++++++++++++++++++++ */ 
-app.use(bodyParser.json());
-app.use(cors());
-
-app.use("/api/cities", citiesRoute);
-
-app.get('/test/', function (req, res) {
-  res.send('HALLO WORLD!');
-});
-
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
-})
+console.log(citiesRoute)
