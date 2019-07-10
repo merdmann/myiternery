e9d249f9e8f3d037b3d59dadd1a3c302ce7f5064
Michael Erdmann <michael.erdmann@snafu.de>
2019-07-10 08:59:04 +0200

    Setup on my xps with fedora

--- client/src/pages/itineraries.js
+++ client/src/pages/itineraries.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Component } from "react";
-import styles from "./itinararies.css"
+import Activity from "../components/activity"
+import styles from "../pages/itinararies.css"
 import Carousel from 'react-images';
-import Collapsible from 'react-collapsible';
- 
-
+import Img from 'react-images';
+Diete
 /* 
  * This comonent is expected to take the city name from http route and tto 
  * list all iternararies for this city
@@ -16,9 +16,8 @@ class Itinararies extends Component {
       urls: null 
     }
   }
-  
    /*
-   * Create the landmrk urls for thr caroucell.
+   * Create the landmrk urls
    */
   createUrls(){
     let landmarks=this.state.itinararies.map(e => e.landmarks)
@@ -32,27 +31,10 @@ class Itinararies extends Component {
     this.setState({urls})
   }
 
-  renderItinary(it) {
-    console.log(styles.infoText)
-    return( <div className="ItinField">
-            <div>By {it.creator.name}</div>
-            <h3>{it.description}</h3>
-            <span>liked {it.liked}</span>
-            <span className="infoText">Likes:{it.liked}</span>
-            <span className="infoText">Duration: {it.duration}hrs </span>
-            <span className="infoText">Cost: {it.cost}$</span><br></br>
-            <span className="infoText">{it.activity.map(t=><span className="tags">{"#"+t}</span>)}</span>
-            <div  className="infoText">Events: {it.landmarks.map( l => <span className="tags">{l.name}</span>)}</div>
-            <Collapsible trigger="View more/View less">
-            {this.state.urls &&  <Carousel  className="infoText" views={this.state.urls}/>}
-            </Collapsible>
-            </div> );
-  }
-
-  render() {
-      return(this.state.itinararies && this.state.itinararies.map(it=>this.renderItinary(it)))
-  }
-
+  /**
+   * THis fetches the itinararies fo a given city given in the 
+   * page arguments city.
+   */
   componentDidMount() {
     fetch("http://localhost:5000/api/tinerary/search?city=" + this.props.match.params.city )
       .then(response =>response.json())
@@ -62,6 +44,31 @@ class Itinararies extends Component {
         this.createUrls()
       });
   };
+
+  /**
+   * 
+   * @param {*} it 
+   * Render a single itinary. 
+   */
+  renderItinary(it) {
+    return( <div>
+            <img src={it.picture}></img>
+            <div>{it.creator.name}</div>
+            <h3>{it.description}</h3>
+            <span>liked {it.liked}</span>
+            <span className="infoText">Likes:{it.liked}</span>
+            <span className="infoText">Duration: {it.duration}hrs </span>
+            <span className="infoText">Cost: {it.cost}$</span><br></br>
+            <span className="infoText">{it.activity.map(t=><span className="tags">{"#"+t}</span>)}</span>
+            <div className="infoText">{it.landmarks.map( l => <span className="tags">{l.name}</span>)}</div>
+            {this.state.urls &&  <Carousel  className="infoText" views={this.state.urls}/>}
+            </div> );
+  }
+
+  render() {
+      return(this.state.itinararies && this.state.itinararies.map(it=>this.renderItinary(it)))
+  }
+
 } /* class */
 
 export default Itinararies;
